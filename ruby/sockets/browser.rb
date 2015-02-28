22d11c980a3d5404e3b8e200de0b69a38a233177
mach1010 <jetstream1010@gmail.com>
2015-02-27 18:02:56 -0600

    finish and refactor server

--- ruby/sockets/browser.rb
+++ ruby/sockets/browser.rb
@@ -1,14 +1,39 @@
 require 'socket'
+require 'json'
 
-host = 'localhost'
-port= 2000
-path = "/index.html"
+$host = 'localhost'
+$port= 2000
+$path = "/index.html"
+$socket = TCPSocket.open($host, $port)
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+def get_method
+  puts "GET or POST?"
+  method = gets.chomp
+  case method
+  when 'GET'
+    $socket.print "GET #{$path} HTTP/1.1\r\n"
+    $socket.print "Date: #{Time.now.ctime}\r\n\r\n"
+  when 'POST'
+    puts 'name?'
+    name = gets.chomp
+    puts 'email?'
+    email = gets.chomp
+    params = {:user => {:name => name, :email => email}}.to_json
+    header = ["POST /thanks.html HTTP/1.1",
+              "Date: #{Time.now.ctime}",
+              "Content-Length: #{params.length}"]
+    $socket.print header.join("\r\n") + "\r\n\r\n"
+    $socket.print params
+  else
+    get_method
+  end
+end
 
-socket = TCPSocket.open(host, port)
-socket.print(request)
-response = socket.read
+get_method
+response = $socket.read
 headers, body = response.split("\r\n\r\n", 2)
-print body
-puts response
+puts headers
+puts body
+#
+# {"user":{"name":"a","email":"a"
+# {"user":{"name":"a","email":"a"}}
--- ruby/sockets/server.rb
+++ ruby/sockets/server.rb
@@ -1,34 +1,123 @@
 require 'socket'
+require 'json'
 
-server = TCPServer.open(2000)
-loop {
-  client = server.accept
-  verb, resource, http = client.gets.split(' ', 3)
-  
-  if File::exists?("./#{resource}")
-    response = File.read("./#{resource}")
-    status = '200 OK'
-  else
-    status = '404 NOT FOUND'
+class Request
+  attr_reader :path, :params, :length
+  def initialize(client)
+    @client = client
+    parse_headers
+    get_params 
+    log_to_console
   end
-  response_line = "HTTP/1.1 #{status}\r\n"
-  response_headers = ['Content-type: text/plain',
-                      "Content-length: #{response.to_s.length || 0}",
-                      "Connection: close",
-                      "Date: #{Time.now.ctime}",
-                      "Server: Ruby"
-                      ]
-
-  client.print response_line
-  client.print response_headers.join("\r\n") + "\r\n\r\n"
-  client.print response
   
-  puts response_line
-  puts   response_headers.join("\r\n") + "\r\n\r\n"
-  puts 'VERB: ' + verb
-  puts 'URI: '  + resource
-  puts 'HTTP: ' + http
-  puts
+  def parse_headers
+    header_string, @headers = '', Hash.new(nil)
+    @method, @path, @http = @client.gets.split(' ', 3)
 
-  client.close
-}
+    while line = @client.gets
+      header_string << line
+      break if header_string =~ /\r\n\r\n$/
+    end
+
+    header_string.split(/\r\n/).each do |line| 
+      pair = line.split(': ')
+      @headers[pair.first] = pair.last
+    end
+  end
+
+  def get_params
+    length = @headers['Content-Length'].to_i
+    @params = JSON.parse(@client.read(length))
+  end
+  
+  def log_to_console
+    puts 'REQUEST:'
+    puts @method, @path, @http, @headers
+    puts @params if @params
+    puts
+  end
+end
+
+class Response
+  attr_reader :resource, :header
+  def initialize(request)
+    @request = request
+    @status, @reason, @length = nil
+    @params = @request.params
+    @resource = render_template
+    determine_status
+    @header = construct_header
+    log_to_console
+  end
+  
+  def render_template
+    resource = get_resource 
+    if @params && resource
+      sub = "<ul><li>Name: #{@params['user']['name']}</li><li> Email: #{@params['user']['email']}</li></ul>"
+      resource.gsub('<%= yield %>', sub)
+    else
+      resource
+    end
+  end
+  
+  def get_resource 
+    File.exists?(resource = ".#{@request.path}") && 
+    !File.directory?(resource) ? File.read(resource) : nil
+  end
+  
+  def determine_status
+    if @resource
+      @status, @reason, @length = '200', 'OK', @resource.length
+    else
+      @status, @reason, @length = '404', 'FILE NOT FOUND', nil
+    end
+  end
+  
+  def construct_header
+    header_info = ["HTTP/1.1", "#{@status}", "#{@reason}", 
+                   "Content-Type: text/html",
+                   "Content-Length: #{@length}",
+                   "Date: #{Time.now.ctime}",
+                   "Server: Ruby",
+                   "Connection: close"]
+    header_info.join("\r\n") + "\r\n\r\n"
+  end
+  
+  def log_to_console
+    puts 'RESPONSE:'
+    puts @header
+    puts 
+  end
+end
+
+class Server
+ def initialize(port)
+   @port = port
+   @client
+   @request
+   @response
+   @socket = TCPServer.open(@port)
+ end
+
+ def listen
+   # listen endlessly
+   loop{
+     # accept a request
+     @client = @socket.accept
+     # parse the request
+     @request = Request.new(@client)
+     # create response based on request
+     @response = Response.new(@request)
+     # send the response
+     @client.print @response.header
+     # IO.copy_stream(@response.resource, @client)
+     @client.print @response.resource
+     # close the transaction
+     @client.close
+   }
+ end
+end
+
+#begin:
+server = Server.new(2000)
+server.listen
